46d313f11793e9ee48a975549e2a177f3cb26463
ebataeva <ebataeva@gmail.com>
2022-05-16 00:39:42 +0300

    login admin page is added ПОПРАВИЛОСЬ

--- tests/test_login_admin_page.py
+++ tests/test_login_admin_page.py
@@ -3,8 +3,8 @@ from selenium.webdriver.common.by import By
 
 def test_login_page_external(driver, main_page_url):
     driver.get(f"{main_page_url}admin")
-    driver.find_element(*login_admin_pages.USERNAME_INPUT)
-    driver.find_element(*login_admin_pages.PASSWORD_INPUT)
-    driver.find_element(*login_admin_pages.SUBMIT_BUTTON)
-    driver.find_element(*login_admin_pages.FORGOTTEN_PASSWORD)
-    driver.find_element(*login_admin_pages.OPENCART_LINK)
+    driver.find_element(*login_admin_pages.LoginAdminPage.USERNAME_INPUT)
+    driver.find_element(*login_admin_pages.LoginAdminPage.PASSWORD_INPUT)
+    driver.find_element(*login_admin_pages.LoginAdminPage.SUBMIT_BUTTON)
+    driver.find_element(*login_admin_pages.LoginAdminPage.FORGOTTEN_PASSWORD)
+    driver.find_element(*login_admin_pages.LoginAdminPage.OPENCART_LINK)
