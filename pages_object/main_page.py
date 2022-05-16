8f46263a567c99ae3a144e84ba0ccbe57f508359
ebataeva <ebataeva@gmail.com>
2022-05-17 01:39:26 +0300

    добавлены страницыз и тесты на них

--- pages_object/main_page.py
+++ pages_object/main_page.py
@@ -4,4 +4,8 @@ from selenium.webdriver.common.by import By
 class MainPage:
     LOGO = (By.CSS_SELECTOR, "#logo")
     SEARCH_BAR = (By.CSS_SELECTOR, "#search")
-    SEARCH_BAR_BUTTON = (By.CSS_SELECTOR, "#input-group-btn")
+    SEARCH_BAR_BUTTON = (By.CSS_SELECTOR, ".input-group-btn")
+    SEARCH_BAR_BUTTON_CART = (By.CSS_SELECTOR, "#cart")
+    SLIDE_1 = (By.CSS_SELECTOR, "#slideshow0")
+    SWIPE_PAGINATOR_BULLET_0 = (By.CSS_SELECTOR, ".swiper-pagination-bullet")
+    SWIPE_PAGINATOR_BULLET_1 = (By.XPATH, "//div[contains(@class, 'swiper-pagination-bullet')]")
--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -12,3 +14,11 @@ def test_header(driver, main_page_url):
     WebDriverWait(driver, 3).until(EC.visibility_of_element_located(main_page.MainPage.LOGO))
     WebDriverWait(driver, 3).until(EC.visibility_of_element_located(main_page.MainPage.SEARCH_BAR))
     WebDriverWait(driver, 4).until(EC.visibility_of_element_located(main_page.MainPage.SEARCH_BAR_BUTTON))
+    WebDriverWait(driver, 4).until(EC.visibility_of_element_located(main_page.MainPage.SEARCH_BAR_BUTTON_CART))
+
+def test_slide(driver, main_page_url):
+    driver.get(main_page_url)
+    WebDriverWait(driver, 3).until(EC.visibility_of_all_elements_located(main_page.MainPage.SLIDE_1))
+    WebDriverWait(driver, 3).until(EC.visibility_of_element_located(main_page.MainPage.SWIPE_PAGINATOR_BULLET_0)).click()
+    WebDriverWait(driver, 3).until(EC.visibility_of_element_located(main_page.MainPage.SWIPE_PAGINATOR_BULLET_1)).click()
+    time.sleep(5)
